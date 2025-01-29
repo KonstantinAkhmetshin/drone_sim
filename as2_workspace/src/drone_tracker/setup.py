3f1c0a3331d0b73de6074d2122117a8cb4404993
CosCos <konstantin.akhmetshin@gmail.com>
2025-01-29 05:49:12 -0800

    build successfully

--- as2_workspace/src/drone_tracker/setup.py
+++ as2_workspace/src/drone_tracker/setup.py
@@ -1,39 +1,64 @@
-from setuptools import setup
+# setup.py
+from setuptools import setup, find_packages
 import os
 from glob import glob
 
 package_name = 'drone_tracker'
 
 setup(
+    # Package information
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
+    packages=find_packages(),
+    
+    # Dependencies
+    install_requires=[
+        'setuptools',
+        'opencv-python',
+        'numpy',
+    ],
+    
+    # Package data and resources
     data_files=[
+        # Package index
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+            
+        # Package manifest
         ('share/' + package_name, ['package.xml']),
-        # Include all launch files
-        (os.path.join('share', package_name, 'launch'), 
-         glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-        # Include all configuration files
+        
+        # Launch files
+        (os.path.join('share', package_name, 'launch'),
+         glob('launch/*launch.[pxy][yma]*')),
+         
+        # Configuration files
         (os.path.join('share', package_name, 'config'), [
             'config/simulation_params.yaml',
-            'config/tracker_params.yaml'
+            'config/tracker_params.yaml',
         ]),
-        # Include all world files
+        
+        # World files
         (os.path.join('share', package_name, 'worlds'),
-         glob(os.path.join('worlds', '*.world'))),
-        # Include all model files
+         glob('worlds/*.world')),
+         
+        # Model files
         (os.path.join('share', package_name, 'models'),
-         glob(os.path.join('models', '*.*'))),
+         glob('models/**/*', recursive=True)),
     ],
-    install_requires=['setuptools'],
+    
+    # Make package zip safe
     zip_safe=True,
+    
+    # Maintainer info
     maintainer='Your Name',
     maintainer_email='your.email@example.com',
     description='Autonomous drone object tracking package',
     license='Apache-2.0',
+    
+    # Tests
     tests_require=['pytest'],
+    
+    # Entry points for ROS2 nodes
     entry_points={
         'console_scripts': [
             'tracker_node = drone_tracker.tracker_node:main',
